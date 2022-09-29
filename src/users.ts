3c242207720027bfdae6b4cf0cd4806f172c09d4
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2022-09-29 15:28:28 -0700

    Update users.ts

--- src/users.ts
+++ src/users.ts
@@ -15,4 +15,5 @@ export const WatchedUsers = [
   '811770910624579584', // French Cat
   '1019641343875760350', //Snip
   '546303073962950657', //selim1337
+  '173557815326015488', // GravityExploitz
 ];
